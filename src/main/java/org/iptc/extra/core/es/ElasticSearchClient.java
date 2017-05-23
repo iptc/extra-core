2d8548730a26970a04d6030d547e198ccbe422ba
manosetro <manosetro@iti.gr>
2017-05-23 16:04:04 +0300

    Fix ES client

--- src/main/java/org/iptc/extra/core/es/ElasticSearchClient.java
+++ src/main/java/org/iptc/extra/core/es/ElasticSearchClient.java
@@ -1,6 +1,8 @@
 package org.iptc.extra.core.es;
 
 import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
@@ -27,7 +29,14 @@ import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
 import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.iptc.extra.core.types.Schema;
+import org.iptc.extra.core.types.Schema.Field;
 import org.iptc.extra.core.types.document.Document;
+import org.iptc.extra.core.types.document.StructuredTextField;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 
 public class ElasticSearchClient {
 
@@ -47,7 +56,7 @@ public class ElasticSearchClient {
 		return findDocuments(qb, indexName, page, nPerPage, null);
 	}
 	
-	public ElasticSearchResponse findDocuments(QueryBuilder qb, String indexName, int page, int nPerPage, List<String> highlightFields) throws IOException {
+	public ElasticSearchResponse findDocuments(QueryBuilder qb, String indexName, int page, int nPerPage, Schema schema) throws IOException {
 		
 		Integer from = (page - 1) * nPerPage;
 		Integer size = nPerPage;
@@ -56,12 +65,13 @@ public class ElasticSearchClient {
 		        .setTypes("documents")
 		        .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
 		        .setQuery(qb)              
-		        .setFrom(from).setSize(size)
+		        .setFrom(from)
+		        .setSize(size)
 		        .setExplain(false);
 		
-		if(highlightFields != null && !highlightFields.isEmpty()) {
+		if(schema != null) {
 			HighlightBuilder hlBuilder = new HighlightBuilder();
-			for(String field : highlightFields) {
+			for(String field : schema.getTextualFieldNames()) {
 				hlBuilder.field(field)
 					.fragmentSize(0)
 					.numOfFragments(0)
@@ -75,33 +85,16 @@ public class ElasticSearchClient {
 		}
 		
 		SearchResponse response = request.get();
-		
+
 		List<Document> documents = new ArrayList<Document>();
 		SearchHits hits = response.getHits();
 		for(SearchHit hit : hits) {
-			Document doc = new Document();
-			doc.setId(hit.getId());
-		
+			String source = hit.sourceAsString();
 			Map<String, HighlightField> highlights = hit.getHighlightFields();
-			Map<String, Object> source = hit.getSource();
-			
-			for(String field : source.keySet()) {
-				if(field.equals("id")) {
-					continue;
-				}
-				
-				String value =  source.get(field).toString();
-				if(highlights.containsKey(field)) {
-					Text[] fragments = highlights.get(field).fragments();
-					if(fragments.length > 0) {
-						value = fragments[0].string();
-					}
-				}
-				
-				doc.addField(field, value);	
+			Document doc = sourceToDocument(source, highlights, schema);
+			if(doc != null) {
+				documents.add(doc);
 			}
-			
-			documents.add(doc);
 		}
 		
 		ElasticSearchResponse resp = new ElasticSearchResponse();
@@ -109,6 +102,48 @@ public class ElasticSearchClient {
 		resp.setFound(hits.getTotalHits());
 		
 		return resp;
+	}
+	
+	public Document sourceToDocument(String source, Map<String, HighlightField> highlights, Schema schema) {
+		Gson gson = new Gson();
+		Reader br = new StringReader(source);
+		JsonObject sourceJson = gson.fromJson(br, JsonObject.class);
+		
+		Document doc = new Document();
+		for(String fieldName : schema.getFieldNames()) {
+			Field schemaField = schema.getField(fieldName);
+			JsonElement fieldValue = sourceJson.get(fieldName);
+			if(fieldValue instanceof JsonPrimitive) {
+				String value = fieldValue.getAsString();
+				
+				if(highlights.containsKey(fieldName)) {
+					Text[] fragments = highlights.get(fieldName).fragments();
+					if(fragments.length > 0) {
+						value = fragments[0].string();
+					}
+				}
+				
+				if(schemaField.hasParagraphs) {
+					StructuredTextField bodyField = new StructuredTextField();
+					bodyField.setValue(value);
+					
+					JsonElement paragraphsArray = sourceJson.get(fieldName + "_paragraphs");
+					if(paragraphsArray != null && paragraphsArray.isJsonArray()) {
+						for(JsonElement paragraphElement : paragraphsArray.getAsJsonArray()) {
+							String paragraph = paragraphElement.getAsJsonObject().get("paragraph").getAsString();
+							bodyField.addParagraph(paragraph);
+						}
+					}
+					
+					doc.addField(fieldName, bodyField);
+				}
+				else {
+					doc.addField(fieldName, value);
+				}
+			}
+		}
+		
+		return doc;
 	}
 	
 	public long countDocuments(QueryBuilder qb, String indexName) throws IOException {
