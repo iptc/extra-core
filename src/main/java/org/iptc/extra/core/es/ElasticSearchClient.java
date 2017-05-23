ef290eb1a8cd58a5d0752fb95c749668c1b6752d
manosetro <manosetro@iti.gr>
2017-05-23 11:01:03 +0300

    Fix ES client

--- src/main/java/org/iptc/extra/core/es/ElasticSearchClient.java
+++ src/main/java/org/iptc/extra/core/es/ElasticSearchClient.java
@@ -100,6 +100,8 @@ public class ElasticSearchClient {
 				
 				doc.addField(field, value);	
 			}
+			
+			documents.add(doc);
 		}
 		
 		ElasticSearchResponse resp = new ElasticSearchResponse();
